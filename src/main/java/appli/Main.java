d0713b2cdc828a187fad50be464c40434783c65d
NOEL-MAURICE <43676449+NOEL-MAURICE@users.noreply.github.com>
2019-06-20 16:10:03 +0200

    Commit

--- src/main/java/appli/Main.java
+++ src/main/java/appli/Main.java
@@ -8,6 +8,8 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 
 import data.DatabaseHandle;
+import data.model.Avatar;
+import data.model.Engin;
 import data.model.Joueur;
 import data.model.Partie;
 
@@ -18,45 +20,11 @@ public class Main {
 		EntityManager em = DatabaseHandle.getEntityManagerFactory();
 		EntityTransaction transaction = em.getTransaction();
 		
-		transaction.begin();
-
-		long nbJoueur = (Long) em.createQuery("Select count(j) from Joueur j").getSingleResult();
-		System.out.println(nbJoueur);
-
-		@SuppressWarnings("unchecked")
-		List<Joueur> lJoueur = em.createQuery("Select j from Joueur j order by j.nom asc").getResultList();
-		for (Joueur j : lJoueur) {
-			System.out.println(j.toString());
-		}
-
-		Partie p1 = new Partie();
-		p1.setNiveau(2);
-		p1.setScore(1500);
-		p1.setDate_(LocalDate.now());
-		em.persist(p1);
-
-		Partie p3 = new Partie();
-		p3.setNiveau(5);
-		p3.setScore(13435);
-		p3.setDate_(LocalDate.of(2018, 03, 02));
-		em.persist(p3);
-
-		TypedQuery<Partie> typedQuery = em.createQuery("from Partie", Partie.class);
-
-		try {
-			typedQuery.getResultList().forEach(e -> e.setNiveau(e.getNiveau() + 1));
-
-		} catch (Exception e) {
-			System.out.println("Erreur");
-		}
-		
 		@SuppressWarnings("unchecked")
 		List<Partie> lPartie = em.createQuery("Select p from Partie p order by p.date_ desc").getResultList();
 		for (Partie p : lPartie) {
 			System.out.println(p.toString());
 		}
-
-		transaction.commit();
 		
 		em.close();
 		System.exit(0);
---
+++ src/main/java/data/model/Avatar.java
@@ -0,0 +1,82 @@
+package data.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Avatar {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@OneToMany(mappedBy="avatar")
+	private Set<Engin> engins ;
+	
+	@OneToOne(mappedBy="avatar")
+	private Joueur joueur;
+	
+	@Column(length = 30)
+	private String nom;
+	
+	@Column(length = 30)
+	private int puissance;	
+	
+	public Avatar(){}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Set<Engin> getEngins() {
+		return engins;
+	}
+
+	public void setEngins(Set<Engin> engins) {
+		this.engins = engins;
+	}
+
+	public Joueur getJoueur() {
+		return joueur;
+	}
+
+	public void setParties(Joueur J) {
+		this.joueur = J;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getPuissance() {
+		return puissance;
+	}
+
+	public void setPuissance(int puissance) {
+		this.puissance = puissance;
+	}
+
+	@Override
+	public String toString() {
+		return "Avatar [id=" + id + ", engins=" + engins + ", puissance="
+				+ puissance + "]";
+	}
+	
+	
+}
